30851b5d331604928d5f671d3ef7e20f3a37677a
abhi040498 <abhishekpandey04041997@gmail.com>
2023-02-07 19:34:28 +0530

    common fun implementation

--- tested_codes/lotterygame.sol
+++ tested_codes/lotterygame.sol
@@ -26,14 +26,29 @@ contract Lottery {
 
     // Now we can use random() % players.length to generate winner. adn this will return a number between 0 and 1-player length.
     function pickWinner() public {
+        // SInce we want that the manager should only call the winning function  we need to provide a condition
+        require(msg.sender == manager);
         uint index = random() % players.length;
         // player[index] is the winner so we can transfer the amount to the winner. for this we use transfer function.
         // since we want to transfer the total balance of the contract to winner so we use this.balance 
         players[index].transfer(this.balance);
-        
+
         // Once the game is over we need to free the array so that new set of plople can join the game and play it.
         // Here value in [x] will create a array of length x adn value of (y) will create an array with initial length y.
-        players = new  address[]();
+        players = new address[](0);
+    }
+
+    // When we want to perform any function that is to be used in multiple places we write a fucntion and at the end of the function body we write
+    // _; This tells the function to execute all the code of the parent function from there. 
+
+    modifier restricted() {
+        require(msg.sender == manager);
+        _;
+    }
+
+    // This function will return all the players in an array.  
+    function getPlayers() public view  returns (address[]) {
+        return players;
     }
 
 }
